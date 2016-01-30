c6979128d3fd6d16b4a1de4f03fb554ddd472553
Heidi <hklee@gmail.com>
2016-01-30 15:46:37 -0800

    New storu

--- stories.php
+++ stories.php
@@ -14,9 +14,16 @@
                 <legend>Snowball</legend>
                 <p>
                     <img src="http://g-ecx.images-amazon.com/images/G/01/img15/pet-products/small-tiles/23695_pets_vertical_store_dogs_small_tile_8._CB312176604_.jpg" alt="Happy Dog" > </br>
-                    We thought we lost it all when the hurricane hit. Our home was destroyed, none of our family heirlooms were salvageable, and we thought we would never see our girl Snowball ever again. After settling into a safe place, we called everyone on our contact list to ask if they had seen our girl. We got one no after another, and lost just about all hope...but then we found paw to heart. They made it so easy to connect with the kind strangers who took our girl in. We are absolutely ecstatic to have our girl back home, and have paw to heart to thank for bringing our family back together!
+                   We thought we lost it all when the hurricane hit. Our home was destroyed, none of our family heirlooms were salvageable, and we thought we would never see our girl Snowball ever again. After settling into a safe place, we called everyone on our contact list to ask if they had seen our girl. We got one no after another and lost just about all hope until we found paw to heart. They made it so easy to connect with the kind strangers who took our girl in. We are absolutely ecstatic to have our girl back home, and have paw to heart to thank for bringing our family back together!
                 </p>
             </fieldset>	
+            <fieldset>
+                <legend>Spike</legend>
+                <p>
+                    <img src="_http://aquafind.com/images/pug.gif" alt="Happy Dog" > </br>
+                    A few months ago, our small Californian town was hit with a series of earthquakes. Spike was pretty spooked and ran all over the house barking at the furniture. When things were calm, I thought it would be ok to open the front door to check the damage outside, but right as I did, we were hit by yet another quake and Spike ran straight out the door... My wife stopped me from running after him because the ground was still shaking, but once we got the radio gave us the official ok, I went outside and spent a good 3 hours looking for him. I didn’t find him, so I called all of our neighbors and all of the animal shelters, but no luck. Turns out, someone a few blocks over found Spike hiding under a bush and took him in to ride out the earthquakes. If they hadn’t posted on paw to heart, we would have never found him! Thank you paw to heart!
+                </p>
+            </fieldset> 
         </div>
 	</body>
 </html>
