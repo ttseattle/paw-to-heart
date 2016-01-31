2bbced9495f654f826c4e42e7da75d02355bdd1c
Christy Pham <christypham@D-108-179-180-42.dhcp4.washington.edu>
2016-01-30 17:00:00 -0800

    dfjkl

--- index.php
+++ index.php
@@ -15,8 +15,7 @@ $matches = $model->getAll();
     <meta charset="UTF-8">
     <title>Paw to Heart</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" href="data/logo2.png">
 </head>
 <body>
